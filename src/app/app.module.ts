fb32da003b0b18d7e6d739edcbdd9ec33faacc02
Brandon McFarland <branmcf@Brandons-MacBook-Pro.local>
2017-09-10 17:36:19 -0500

    Created angular4 app and and replaced contents of /src with contents of InSecurity-Web-Frontend staging branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ResultComponent } from './components/result/result.component';
+import { NetworkReportComponent } from './components/networkReport/networkReport.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { DeviceReportComponent } from './components/deviceReport/deviceReport.component';
+
+const appRoutes: Routes = [
+  { path: 'result', component: ResultComponent },
+  { path: 'landing', component: LandingComponent },
+  { path: 'deviceReport', component: DeviceReportComponent },
+  { path: 'NetworkReportComponent', component: NetworkReportComponent },
+  { path: '',
+    redirectTo: '/landing',
+    pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ResultComponent, NetworkReportComponent, LandingComponent, DeviceReportComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/deviceReport/deviceReport.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'device-report',
+  templateUrl: './deviceReport.component.html',
+  styleUrls: ['./deviceReport.component.css']
+})
+export class DeviceReportComponent {
+  
+}
